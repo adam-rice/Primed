d4a1349279c3218f75db94e3243cd272bf05e5e4
Adam Rice <adamcoltonrice@gmail.com>
2017-02-19 15:38:06 -0700

    Set up controllers for home and forecast

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 /*jshint esversion: 6 */
 
 const primed = angular.module('primed', ['ngRoute', 'ngResource']);
+
+primed.controller('homeController', ['$scope', function($scope) {
+
+}]);
+
+primed.controller('forecastController', ['$scope', function($scope) {
+
+}]);
