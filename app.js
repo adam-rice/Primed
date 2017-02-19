db16a18f34e4f50d86d34c7af99fd20baa949634
Adam Rice <adamcoltonrice@gmail.com>
2017-02-19 15:42:04 -0700

    Route provider to app.js

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 
 const primed = angular.module('primed', ['ngRoute', 'ngResource']);
 
+primed.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'pages/home.html',
+    controller: 'homeController'
+  })
+
+  .when('/forecast', {
+    templateUrl: 'pages/forecast.html',
+    controller: 'forecastController'
+  });
+});
+
 primed.controller('homeController', ['$scope', function($scope) {
 
 }]);
