4d677fad67f38d29f319dc07e89f4b3833f0b75d
Adam Rice <adamcoltonrice@gmail.com>
2017-02-20 16:31:25 -0700

    Setup .when for new route forecast/:day

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 var primed = angular.module('primed', ['ngRoute', 'ngResource']);
 
+//ROUTES
 primed.config(function($routeProvider) {
+    
   $routeProvider
-  .when('/', {
-    templateUrl: 'pages/home.html',
-    controller: 'homeController'
-  })
+      .when('/', {
+      templateUrl: 'pages/home.html',
+      controller: 'homeController'
+      })
 
-  .when('/forecast', {
-    templateUrl: 'pages/forecast.html',
-    controller: 'forecastController'
-  });
+      .when('/forecast', {
+      templateUrl: 'pages/forecast.html',
+      controller: 'forecastController'
+      })
+    
+      .when('/forecast/:days', {
+      templateUrl: 'pages/forecast.html',
+      controller: 'forecastController'
+      });
+    
 });
 
-//SERVICE
+//SERVICES
 primed.service('cityService', function() {
     
     this.city = "Denver";
