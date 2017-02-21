77b002d7131bbcb173d125ca34a6c92ef0fc8b07
Adam Rice <adamcoltonrice@gmail.com>
2017-02-20 23:38:35 -0700

    Modularize directives

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
 var primed = angular.module('primed', ['ngRoute', 'ngResource']);
 
-//DIRECTIVES
-primed.directive('daily', function() {
-    
-    return {
-        restrict: 'E',
-        templateUrl: 'directives/daily.html',
-        replace: true,
-        scope: {
-            day: '=',
-            round: '&',
-            convertToDate: '&',
-            dateFormat: '@'
-        }
-    }
-    
-});
-
 
 
 
