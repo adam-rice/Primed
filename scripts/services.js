5b1d14ead3fca2798fc463b3953d2f542a63fdba
Adam Rice <adamcoltonrice@gmail.com>
2017-02-22 10:23:12 -0700

    Create gifService Service

--- scripts/services.js
+++ scripts/services.js
@@ -4,3 +4,10 @@ primed.service('cityService', function() {
 //    this.city = "denver, CO";
     
 });
+
+primed.service('gifService', function() {
+    
+    
+//    this.theme = 'bears';
+    
+});
